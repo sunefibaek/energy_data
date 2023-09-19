daaaf206657c6d6af00971220dab1d968365a4a9
Sune Fibaek <sune.bugge.fibaek@lego.com>
2023-09-19 22:41:00 +0200

    logic to handle lack of solarpower deleted a lot of debugging stuff

--- sources_sankey.py
+++ sources_sankey.py
@@ -10,49 +10,33 @@ result = response.json()
 records = result.get('records', [])
 
 df = pd.json_normalize(records)
-print(df)
 
 title = "Danish Energy Production at " + df['Minutes5DK'].iloc[0]
 
 df['Non-Sustainable Sources'] = df['ProductionLt100MW'] + df['ProductionGe100MW']  
 df_prep = df.drop(["ProductionGe100MW","ProductionLt100MW","PriceArea","Minutes5UTC", "ExchangeNetherlands","ExchangeNorway","ExchangeSweden","BornholmSE4","ExchangeGreatBelt","ExchangeGermany"], axis=1)
-print(df_prep)
 
 df = df_prep.sum(numeric_only=True).to_frame().T
 df['PriceArea'] = "All"
-print(df)
 
 df['WindPower'] = df['OffshoreWindPower'] + df['OnshoreWindPower']  
 df['Sustainable Sources'] = df['OffshoreWindPower'] + df['OnshoreWindPower'] + df['SolarPower']  
-print(df) 
 
 df_sustanability = df[['PriceArea', 'Sustainable Sources', 'Non-Sustainable Sources']].melt(id_vars='PriceArea', value_vars=['Sustainable Sources', 'Non-Sustainable Sources'], var_name='target', value_name='value')
 df_sustanability['source'] = 'Total Production'
-print(df_sustanability)
 
 df_sustain_split = df[['PriceArea', 'WindPower', 'SolarPower']].melt(id_vars='PriceArea', value_vars=['WindPower', 'SolarPower'], var_name='target', value_name='value')
 df_sustain_split['source'] = 'Sustainable Sources'
-print(df_sustain_split)
 
 df_wind_split = df[['PriceArea', 'OffshoreWindPower', 'OnshoreWindPower']].melt(id_vars='PriceArea', value_vars=['OffshoreWindPower', 'OnshoreWindPower'], var_name='target', value_name='value')
 df_wind_split['source'] = 'WindPower'
-print(df_wind_split)
 
 df_sankey = pd.concat([df_sustanability, df_sustain_split, df_wind_split])[['source', 'target', 'value']]
-print(df_sankey)
 
 df_sankey['source_target'] = df_sankey['source'] + df_sankey['target']
-print(df_sankey)
 
 labels = list(df_sankey['source'].unique()) + list(df_sankey['target'].unique())
 labels = list(set(labels))
-print(labels)
-print([labels.index(s) for s in df_sankey['source']])
-print([labels[i] for i in [labels.index(s) for s in df_sankey['source']]])  
-
-print([labels.index(s) for s in df_sankey['target']])
-print([labels[i] for i in [labels.index(s) for s in df_sankey['target']]])  
-
 
 color_array = [
     'rgba(0, 0, 0, 0.6)' if 'Non-Sustainable Sources' in s
@@ -62,13 +46,20 @@ color_array = [
     for s in df_sankey['source_target']
 ]
 
+if (df_sankey.loc[df_sankey['target'] == 'SolarPower', 'value'] == 0).any():  
+    x_axis = [0.8, 0.2, 0.8, 0.6, 0.4, 0.8]
+    y_axis = [0.1, 0.4, 0.2, 0.3, 0.3, 0.4]
+else:  
+    x_axis = [0.8, 0.2, 0.8, 0.8, 0.6, 0.4, 0.8]
+    y_axis = [0.1, 0.4, 0.2, 0.3, 0.3, 0.3, 0.4]
+
 fig = go.Figure(data=[go.Sankey(
     arrangement = "snap",
     
     node = dict(
         label=labels,
-        x=[0.8, 0.2, 0.8, 0.8, 0.6, 0.4, 0.8],
-        y=[0.1, 0.4, 0.2, 0.3, 0.3, 0.3, 0.4],
+        x = x_axis, #x=[0.8, 0.2, 0.8, 0.8, 0.6, 0.4, 0.8],
+        y = y_axis, #y=[0.1, 0.4, 0.2, 0.3, 0.3, 0.3, 0.4],
 # Index Label                       X       Y
 # 0     OffshoreWindPower         0.4     0.1
 # 1     Total Production          0.1     0.4
